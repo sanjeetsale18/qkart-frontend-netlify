3a94bc6a89779c18e8f6c4c508db1233a099026e
sanjeetsale18 <80786320+sanjeetsale18@users.noreply.github.com>
2022-03-11 00:27:59 +0530

    initialise project

---
+++ src/App.js
@@ -0,0 +1,36 @@
+import Register from "./components/Register";
+import ipConfig from "./ipConfig.json";
+import { Route, Switch } from "react-router-dom";
+import Login from "./components/Login";
+import Products from "./components/Products";
+import theme from "./theme"
+import { ThemeProvider } from "@mui/system";
+import React from "react";
+import Checkout from "./components/Checkout"
+import Thanks from "./components/Thanks"
+
+
+export const config = {
+  endpoint: `https://qkart-frontend.herokuapp.com/api/v1`,
+};
+
+function App() {
+  return (
+    <div className="App">
+      {/* TODO: CRIO_TASK_MODULE_LOGIN - To add configure routes and their mapping */}
+      <React.StrictMode>
+      <ThemeProvider theme={theme}>
+      <Switch>
+             <Route path="/register" component={Register} />
+             <Route path="/login" component={Login} />
+             <Route exact path="/" component={Products} />
+             <Route path="/checkout" component={Checkout} />
+             <Route path="/thanks" component={Thanks} />
+        </Switch>
+      </ThemeProvider>
+      </React.StrictMode>  
+    </div>
+  );
+}
+
+export default App;
